f85a750d465a31ff1f8ae4bdb5e9d9d1430be45e
Yogesh Bhatt <bhattyogesh700@gmail.com>
2022-02-07 02:28:26 +0200

    json data created

--- scrape.py
+++ scrape.py
@@ -6,4 +6,6 @@ len(dfs)
 df = dfs[0]
 df.head
 df
+df.drop(['LTV','Quantity','Difference Rs.','No of Transaction'], axis=1, inplace=True)
+df.rename(columns={'Closing Price' : 'Closing', 'Max Price' : 'High', 'Min Price' : 'Low', 'Opening Price' : 'Open', '%Change':'Change'}, inplace=True)
 df.to_excel('output.xlsx')
