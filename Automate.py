d75103117acad0ce77aca23bd8e319b64c08ed2a
Yogesh Bhatt <bhattyogesh700@gmail.com>
2022-02-13 11:41:17 +0200

    No excel file anymore, data directly to json

--- Automate.py
+++ Automate.py
@@ -1,12 +1,11 @@
-import os, sys
-import time
+import os, sys, time
 
 #i = 0
 #while i < 1:
 os.system("scrape.py")
-print('data acquired')
-time.sleep(5)
-os.system("converter.py")
+#print('data acquired')
+#time.sleep(5)
+#os.system("converter.py")
 print('Data converted')
     #i += 1
 
--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+outpath='data.json'
+
 if (os.path.exists('data.json')):
     os.remove('data.json')
 dfs = pd.read_html('https://www.nepalipaisa.com/StockLive.aspx')
@@ -10,5 +12,28 @@ df.head
 df
 df.drop(['LTV','Quantity','Difference Rs.','No of Transaction'], axis=1, inplace=True)
 df.rename(columns={'Closing Price' : 'Closing', 'Max Price' : 'High', 'Min Price' : 'Low', 'Opening Price' : 'Open', '%Change':'Change'}, inplace=True)
-print(df)
-df.to_excel('data.xlsx')
+#print(df)
+#df.to_excel('data.xlsx')
+print('Data acquired')
+Symbols = df.Symbols
+Closing = df.Closing
+Change = df.Change
+High = df.High
+Low = df.Low
+Open = df.Open
+
+container = { }
+
+i = 0
+while i< len(Symbols):
+    container[Symbols[i]] = [
+        {"Open:": Closing[i]},
+        {"High:": High[i]},
+        {"Low:": Low[i]},
+        {"Closing Price:": Closing[i]},
+        {"Change:": Change[i]}
+    ]
+    i = i +1
+df = pd.DataFrame(container)
+df.to_json(outpath, indent=4)
+print('Data converted')
