d49c43630728c9f8f829451ced8342b4bcfaae57
Yogesh bhatt <bhattyogesh700@gmail.com>
2022-04-01 23:21:46 +0300

    .

--- Automate.py
+++ Automate.py
@@ -9,9 +9,9 @@ def countdown(t):
         time.sleep(1)
         t -= 1
 
-os.system("scrape.py")
-print("Starting webserver in:")
-#countdown(int(5))
-#time.sleep(5)
-#os.system("converter.py")
-os.system("main.py")
+value = True
+while (value):
+    os.system("scrape.py")
+    print("Data will refresh in:")
+    countdown(int(10))
+    
