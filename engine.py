0d6f6c8d67d98746ab0b2fe621cdb2cb399c71e6
Yogesh bhatt <bhattyogesh700@gmail.com>
2023-05-09 00:36:13 +0300

    new scraping engine for new URL

--- engine.py
+++ engine.py
@@ -1,23 +1,56 @@
-import pandas as pd
 import os, json, sqlite3, datetime, time
 from sqlalchemy import true
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 outpath='data\data.json'
 def scrape():
     if (os.path.exists(outpath)):
         os.remove(outpath)
+    # Set Chrome options to run headless
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    # Initialize the webdriver
+    driver = webdriver.Chrome(options=chrome_options)
+    # Open the webpage
+    driver.get("https://www.nepalipaisa.com/live-market")
+    # Wait for the table to load
+    driver.implicitly_wait(10)
+    # Get the page source
+    html = driver.page_source
+    # Close the webdriver
+    driver.quit()
+    # Parse the html using BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+    # Find the table
+    table = soup.find('table', {'class': 'table table-responsive'})
+    # Extract data from the table
+    headers = ["Symbols","Closing","Chg","Change","High","Low","Open","Qty","Txn *"]
+    rows = []
+    for row in table.find_all('tr'):
+        rows.append([cell.text.strip() for cell in row.find_all('td')])
 
-    dfs = pd.read_html('https://www.nepalipaisa.com/StockLive.aspx')
-    len(dfs)
-    df = dfs[0]
-    df.head
-    df
-    df.drop(['LTV','Quantity','Difference Rs.','No of Transaction'], axis=1, inplace=True)
-    df.rename(columns={'Closing Price' : 'Closing', 'Max Price' : 'High', 'Min Price' : 'Low', 'Opening Price' : 'Open', '%Change':'Change'}, inplace=True)
-    #print(df)
-    df.to_json(outpath, indent=4,orient='records')
-    return df
+    # Combine headers and data into a list of dictionaries
+    data = []
+    for row in rows[1:]:
+        data.append(dict(zip(headers, row)))
+
+    #Convert string numbers in float values
+    for row in data:
+        row['Closing'] = float(row['Closing'].replace(',', ''))
+        row['Chg'] = float(row['Chg'].replace(',', ''))
+        row['Change'] = float(row['Change'].replace(',', ''))
+        row['High'] = float(row['High'].replace(',', ''))
+        row['Low'] = float(row['Low'].replace(',', ''))
+        row['Open'] = float(row['Open'].replace(',', ''))
+        row['Qty'] = float(row['Qty'].replace(',', ''))
+        row['Txn *'] = float(row['Txn *'].replace(',', ''))
+
+    # Save data to JSON file
+    with open(outpath, 'w') as f:
+        json.dump(data, f, indent=4)
 
 def read_nepse_data():
     scrape()
@@ -25,6 +58,7 @@ def read_nepse_data():
         nepse = json.load(d)
         return nepse 
 
+#below this is experimental and contains heavy bugs and unnecessary code.
 conn = sqlite3.connect('data.db')
 date = datetime.datetime.now().strftime("%x")
 table_name = f"T_{date.replace('/', '_')}"
@@ -34,7 +68,6 @@ def set_database():
     c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (symbols text, Closing number, Change number, High number, Low number, Open number)")
     conn.commit()
 
-#below this is experimental and contains heavy bugs and unnecessary code.
 def data_to_database():
     while true:
         set_database()
